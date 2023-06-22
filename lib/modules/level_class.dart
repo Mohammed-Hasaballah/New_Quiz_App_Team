50d1e4fc8978fa17264361dfd5392935c4f0fa1a
AhmadHerzalla <ahmadakramherzalla@gmail.com>
2023-06-22 16:12:18 +0300

    add level class

---
+++ lib/modules/level_class.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+
+class level{
+   late final String title;
+   late final String subtitle;
+   late final String? descreption;
+  late final String? image;
+  late final IconData? icon;
+  late final List<Color>colors;
+  late final String routeName;
+  level({
+    required this.title,
+    required this.subtitle,
+     this.descreption,
+     this.image,
+     this.icon,
+     required  this.colors,
+    required this.routeName,
+  });
+
+}
+
+
+
